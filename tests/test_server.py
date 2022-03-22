e980bb043d6aa97f6f5b6ff7081e49f9491984f4
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-22 21:25:35 +0900

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,13 +2,25 @@ from logging_server.server import LoggingServer
 import logging
 import sys
 import time
-
+ls = LoggingServer(port=8316)
 def test_start():
-    ls = LoggingServer()
+    global ls
     sh = logging.StreamHandler(sys.stdout)
     ls.logger.addHandler(sh)
     ls.logger.setLevel(0)
     ls.start()
     time.sleep(0.5)
     ls.shutdown()
-    
+
+
+def test_delete_shutdown():
+    global ls
+    ls.start()
+    time.sleep(0.5)
+    del ls
+
+def test_auto_shutdown():
+    ls = LoggingServer(port=12345)
+    ls.start()
+    time.sleep(0.5)
+
