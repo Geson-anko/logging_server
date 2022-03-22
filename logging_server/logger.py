4f42e710a7daa2c5222a2d8269695457441ec40d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-22 22:25:56 +0900

    Create SocketLogger class feature

--- logging_server/logger.py
+++ logging_server/logger.py
@@ -17,3 +17,70 @@ Usage:
 ロギングに必要なメソッドのみを提供します。
 また任意のハンドラーを追加しても正常に機能しないことがあります。
 """
+import os
+import logging
+import logging.handlers
+
+def _check_pid(func):
+    def check(self,*args, **kwds):
+        pid = os.getpid()
+        if self.__pid != pid:
+            self.__pid = pid
+            self.reset_logger()
+        func(self,*args, **kwds)
+    return check
+class SocketLogger:
+    __pid:int = None
+    __logger:logging.Logger = None
+    def __init__(
+        self, name:str, level:logging.NOTSET, host="localhost",
+        port:int=logging.handlers.DEFAULT_TCP_LOGGING_PORT,
+    ) -> None:
+        self.__pid = os.getpid()
+        self.name = name
+        self.level = level
+        self.host = host
+        self.port = port
+        self.set_logger()
+
+    @property
+    def logger(self):
+        return self.__logger
+
+    def set_logger(self):
+        """set logger class, name, level and socket handler."""
+        self.__logger = logging.getLogger(self.name)
+        self.__logger.setLevel(self.level)
+        socket_handler = logging.handlers.SocketHandler(self.host, self.port)
+        socket_handler.setLevel(logging.NOTSET)
+        self.__logger.addHandler(socket_handler)
+        self.__logger.propagate=False # Because another logger is propagating in server process.
+
+    def remove_handlers(self):
+        """remove handlers of logger."""
+        for hdlr in self.__logger.handlers:
+            self.__logger.removeHandler(hdlr)
+
+    def reset_logger(self):
+        """reset logger class"""
+        self.remove_handlers()
+        self.set_logger()
+
+    @_check_pid
+    def debug(self,*args, **kwds) -> None: self.logger.debug(*args, **kwds)
+    @_check_pid
+    def info(self,*args, **kwds) -> None: self.logger.info(*args, **kwds)
+    @_check_pid
+    def warn(self,*args, **kwds) -> None: self.logger.warn(*args, **kwds)
+    @_check_pid
+    def warning(self,*args, **kwds) -> None: self.logger.warning(*args, **kwds)
+    @_check_pid
+    def error(self,*args, **kwds) -> None: self.logger.error(*args, **kwds)
+    @_check_pid
+    def critical(self,*args, **kwds) -> None: self.logger.critical(*args, **kwds)
+    @_check_pid
+    def exception(self,*args, **kwds) -> None: self.logger.exception(*args, **kwds)
+    
+
+
+
