9984f5041faec723cf33858ad7ab49645e187768
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-03-22 20:50:02 +0900

    Added Logging Server class
    copied from https://docs.python.org/ja/3/howto/logging-cookbook.html#sending-and-receiving-logging-events-across-a-network

---
+++ logging_server/server.py
@@ -0,0 +1,25 @@
+from .handlers import LogRecordStreamHandler
+import socketserver
+import logging
+import logging.handlers
+
+class LoggingServer(socketserver.ThreadingTCPServer):
+    """The SocketServer which receive Logs."""
+
+    allow_reuse_address = True
+
+    def __init__(self,host='localhost',port=logging.handlers.DEFAULT_TCP_LOGGING_PORT, 
+                handler=LogRecordStreamHandler):
+        super().__init__((host, port), handler)
+        self.abort = 0
+        self.timeout = 1
+        self.logname = None
+    
+    def serve_until_stopped(self,shutdown):
+        import select
+        abort = 0
+        while not abort and not shutdown.value:
+            rd, wr, ex = select.select([self.socket.fileno()], [], [], self.timeout)
+            if rd:
+                self.handle_request()
+            abort = self.abort
