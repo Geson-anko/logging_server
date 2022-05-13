c8434c5cdec4547c92221a946b4be1bc14457d91
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-13 14:33:15 +0900

    Fixed test_logging

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,26 +2,15 @@ from logging_server.logger import SocketLogger
 from logging_server.server import LoggingServer
 import logging
 import sys
+import time
+import logging
 
-msg = """\033[31mYou must see below message after test.\033[0m
-About starting LoggingServer...
-start test logging
-debug
-info
-warning
-error
-critical
+ls = LoggingServer()
+ls.start()
 
-Traceback (most recent call last):
-  File "../logging_server/test_logger.py", line 17, in test_logging
-    raise Exception
-Exception
-\033[31mYou must see above message after test.\033[0m
-"""
-
-def test_logging():
-    print(msg)
-    print("start test logging")
+def _test_logging():
+    #print(msg)
+    #print("start test logging")
     logger = SocketLogger("test_logging")
     logger.debug("debug")
     logger.info("info")
@@ -31,8 +20,25 @@ def test_logging():
     try:
         raise Exception
     except Exception as e:
-        pass
         logger.exception(e)
+
+def test_logging(caplog):
+    caplog.set_level(0)
+    _test_logging()
+    time.sleep(0.01)
+    d,i,w,e,c,exc = caplog.records
+    assert d.levelname == "DEBUG"
+    assert d.msg == "debug"
+    assert i.levelname == "INFO"
+    assert i.msg == "info"
+    assert w.levelname == "WARNING"
+    assert w.msg == "warning"
+    assert e.levelname == "ERROR"
+    assert e.msg == "error"
+    assert c.levelname == "CRITICAL"
+    assert c.msg == "critical"
+    assert exc.levelname == "ERROR"
+    assert exc.msg == ""
 
 
 if __name__ == "__main__":
