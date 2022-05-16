07f2e7b8f6d3060eb775f92db4012dd6924da8b6
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-16 17:23:11 +0900

    Update test_logger.py
    test_logging using logcap.record_tuples

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,6 +5,7 @@ import sys
 import time
 import logging
 import os
+from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 PORT = 10001
 ls = LoggingServer(port=PORT)
@@ -43,19 +44,16 @@ def test_logging(caplog):
     caplog.set_level(0)
     _test_logging()
     time.sleep(0.01)
-    d,i,w,e,c,exc = caplog.records
-    assert d.levelname == "DEBUG"
-    assert d.msg == "debug"
-    assert i.levelname == "INFO"
-    assert i.msg == "info"
-    assert w.levelname == "WARNING"
-    assert w.msg == "warning"
-    assert e.levelname == "ERROR"
-    assert e.msg == "error"
-    assert c.levelname == "CRITICAL"
-    assert c.msg == "critical"
-    assert exc.levelname == "ERROR"
-    assert exc.msg == ""
+
+    
+    rec_tup = caplog.record_tuples
+
+    assert ("test_logging", DEBUG, "debug") in rec_tup
+    assert ("test_logging", INFO, "info") in rec_tup
+    assert ("test_logging", WARNING, "warning") in rec_tup
+    assert ("test_logging", ERROR, "error") in rec_tup
+    assert ("test_logging", CRITICAL, "critical") in rec_tup
+    assert ("test_logging", ERROR, "") in rec_tup
 
 
 if __name__ == "__main__":
