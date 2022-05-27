bc80097b7d60e6d49eab6e313cde8cc87a00c56f
Subaru8556 <tkmkn6100@icloud.com>
2022-05-27 09:37:14 +0900

    fix testfile

--- python-tests.bat
+++ python-tests.bat
@@ -1,1 +1,5 @@
-poetry run pflake8 .\logging_server\ & poetry run black .\logging_server\ & poetry run isort .\logging_server\ & poetry run mypy .\logging_server\ & poetry run pytest .\tests
+poetry run pflake8 .\logging_server &^
+poetry run black .\logging_server &^
+poetry run isort .\logging_server &^
+poetry run mypy .\logging_server &^
+poetry run pytest .\tests
